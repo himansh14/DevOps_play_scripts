b9e0d90a9d4de3118f2ea6f116885f01c71008bd
Himanshu S Waghale <108952827+himansh14@users.noreply.github.com>
2023-02-23 10:58:52 +0530

    Update cpu_metrics.sh

--- cpumetrics/cpu_metrics.sh
+++ cpumetrics/cpu_metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+(blah blah blah)
 # To collect CPU, Memory and Disk usage report from slave (devops minion) and send alert to master (devops play) server
-# Format : Hostname, Date&Time, CPU%, MEM%, DiskUsage% blah blah blah 
+# Format : Hostname, Date&Time, CPU%, MEM%, DiskUsage% 
 
 i=$(cat /opt/shell_scripts/cpumetrics/hostlist)
 
